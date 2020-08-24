10453fb0739610c3f187477fcb7cef4e24d9fb51
DhanrajVedanth <dhanrajvedanth08@gmail.com>
2020-08-24 11:52:37 -0400

    Update test_working.rb

--- Ruby/test_working.rb
+++ Ruby/test_working.rb
@@ -1,7 +1,18 @@
-x = 12
+require "test/unit"
+require_relative 'game_of_life'
 
-if x <= 11
-    print("Hey")
-else
-    print("Otha")
+class GameOfLifeTest < Test::Unit::TestCase
+  def test_change
+    testInput = [[0,0,0], [1,1,1], [0,0,0]]
+    expectedOutput = [[0,1,0],[0,1,0],[0,1,0]]
+    output = game_of_life(testInput)
+    assert_equal output, expectedOutput, "The output is different form the expected output"
+  end
+
+  def test_noChange
+    testInput = [[1,1], [1,1]]
+    expectedOutput = [[1,1], [1,1]]
+    output = game_of_life(testInput)
+    assert_equal output, expectedOutput, "The output is different form the expected output"
+  end
 end
